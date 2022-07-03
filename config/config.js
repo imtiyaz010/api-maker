11f2101e5fbe157d451cb71b73dd09d8aedaf64d
imtiyaz01 <bhupendra.singh@crownstack.com>
2022-07-03 12:07:07 +0530

    set mongo atlas

--- config/config.js
+++ config/config.js
@@ -1,10 +1,5 @@
-/*const config = {
-    MONGODB_URL: 'mongodb://localhost:27017',
-    DATABASE_NAME: 'apimaker'
-}*/
-
 const config = {
-    MONGODB_URL: `mongodb+srv://imtiyazj:NuctwvdWCGRWkwuQ@cluster0.vl7oh.mongodb.net/?retryWrites=true&w=majority`,
+    MONGODB_URL: `${process.env.MONGODB_URL} || mongodb://localhost:27017`,
     DATABASE_NAME: 'apimaker'
 }
 
