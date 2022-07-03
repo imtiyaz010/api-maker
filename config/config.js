ecde577922495fc7e849b74d17ab89910cda46f0
imtiyaz01 <bhupendra.singh@crownstack.com>
2022-07-03 12:29:19 +0530

    make it better

--- config/config.js
+++ config/config.js
@@ -1,5 +1,10 @@
+/*const config = {
+    MONGODB_URL: 'mongodb://localhost:27017',
+    DATABASE_NAME: 'apimaker'
+}*/
+
 const config = {
-    MONGODB_URL: `${process.env.MONGODB_URI} || mongodb://localhost:27017`,
+    MONGODB_URL: `mongodb+srv://imtiyazj:NuctwvdWCGRWkwuQ@cluster0.vl7oh.mongodb.net/?retryWrites=true&w=majority`,
     DATABASE_NAME: 'apimaker'
 }
 
