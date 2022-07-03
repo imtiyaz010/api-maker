94eee79efbb1b77b4f0930fa27b17aa861e51381
imtiyaz01 <bhupendra.singh@crownstack.com>
2022-07-03 12:20:05 +0530

    updated URL to URI

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 const config = {
-    MONGODB_URL: `${process.env.MONGODB_URL} || mongodb://localhost:27017`,
+    MONGODB_URL: `${process.env.MONGODB_URI} || mongodb://localhost:27017`,
     DATABASE_NAME: 'apimaker'
 }
 
